1935296a6f76003c86650c28e2c16a77535d758c
piotrek <kip1011001@gmail.com>
2017-02-26 14:00:07 +0100

    first commit

---
+++ WebDriverFrameworkUnitTests/PageObject.cs
@@ -0,0 +1,21 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverFrameworkUnitTests
+{
+    public class PageObjectExample
+    {
+        public PageObjectExample()
+        {
+            PageFactory.InitElements(PropertiesCollection.driver, this);
+        }
+
+        [FindsBy(How=How.ClassName, Using = "group-button")]
+        public IWebElement Group_ButtonClassName { get; set; }
+    }
+}
---
+++ WebDriverFrameworkUnitTests/PropertiesCollection.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebDriverFrameworkUnitTests
+{
+    public class PropertiesCollection
+    {
+        public enum PropertyType
+        {
+            Id,
+            Name,
+            ClassName,
+            LinkText,
+        }
+
+        // interface through which  the use controls the browser in any method of my code
+        public static IWebDriver driver { get; set; }
+    }
+}
